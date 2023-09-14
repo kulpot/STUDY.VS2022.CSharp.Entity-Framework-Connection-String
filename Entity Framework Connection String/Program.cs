cc11ae8c8fc9dbd52db7fd178fb3c4ec23df1d7e
Kulpot <kulpotwahu@gmail.com>
2023-09-15 05:43:46 +0800

    Add project files.

---
+++ Entity Framework Connection String/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+//ref link:https://www.youtube.com/watch?v=EG3GykWhuR8&list=PLRwVmtr-pp06bXl6mbwDfK1eW9sAIvWUZ&index=3
+// EntityFramework - is a object relational mapping(ORM) data tool
+// SQL Server Management Studio - Database app
+// SQL Server Profiler - app for tracing SQL Database
+
+class Video
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+class MeContext : DbContext
+{
+    //public MeContext() : base(@"Data Source=.;Initial Catalog=KulpotDB;Integrated Security=True")
+    public MeContext() : base(@"Data Source=.;Initial Catalog=MyTestDb;Integrated Security=True")
+    {
+
+    }
+    public DbSet<Video> Videos { get; set; }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        var vid = new Video
+        {
+            Title = "Hello World Entity Framework",
+            Description = "Learn about the entity framework"
+        };
+        var meContext = new MeContext();
+
+        //meContext.Database.Delete();
+
+        meContext.Videos.Add(vid);
+        meContext.SaveChanges();
+    }
+}
